7110152f8757e24bad2b79eaa35382c08a5b5b72
Ilya V. Schurov <ilya@schurov.com>
2024-02-09 16:01:28 +0100

    Added kill arrayjob feature

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="slurmtoppy",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         # List your project's dependencies here
         # e.g., 'requests>=2.22.0'
